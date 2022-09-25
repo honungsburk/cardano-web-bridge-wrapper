c774c4401b6d361d0fbb6a7b394122260a713c8f
Frank Hampus Weslien <Frank.Weslien@geshdo.com>
2022-09-25 07:24:37 +0200

    Add comment explaining AccountChange API error

--- src/errors/APIError.ts
+++ src/errors/APIError.ts
@@ -4,6 +4,9 @@ import { WebBridgeError } from './WebBridgeError';
  * - InvalidRequest - Inputs do not conform to this spec or are otherwise invalid.
  * - InternalError - An error occurred during execution of this API call.
  * - Refused - The request was refused due to lack of access - e.g. wallet disconnects.
+ * - AccountChange - The account has changed. The dApp should call wallet.enable()
+ *   to reestablish connection to the new account. The wallet should not ask for
+ *   confirmation as the user was the one who initiated the account change in the first place
  */
 export enum APIErrorCode {
   InvalidRequest = -1,
